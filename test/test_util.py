a5b1bdcaafc9ec7235c2996fa101ddb9ff878939
Alice Bevan–McGregor <alice@gothcandy.com>
2014-10-09 16:15:30 -0400

    Additonal tests.

--- test/test_util.py
+++ test/test_util.py
@@ -10,17 +10,32 @@ from textwrap import TextWrapper
 from marrow.task.util import resolve, lookup
 
 
-class TestResolve(object):
+class TestResolveStdlib(object):
 	def test_basic(self):
 		assert resolve(chain) == 'itertools:chain'
 	
 	def test_class(self):
 		assert resolve(TextWrapper) == 'textwrap:TextWrapper'
 	
-	@pytest.mark.skipif(bool(os.environ.get('CANARY', False)), reason="No im_class or __qualname__ support.")
+	# @pytest.mark.skipif(bool(os.environ.get('CANARY', False)), reason="No im_class or __qualname__ support.")
 	def test_method(self):
 		assert resolve(TextWrapper.fill) == 'textwrap:TextWrapper.fill'
+
+
+class TestResolveMarrowTask(object):
+	def test_class(self):
+		from marrow.task.model.task import Task
+		assert resolve(Task) == 'marrow.task.model.task:Task'
 	
+	def test_method(self):
+		from marrow.task.model.task import Task
+		assert resolve(Task().acquire) == 'marrow.task.model.task:Task.acquire'
+		assert resolve(Task.acquire) == 'marrow.task.model.task:Task.acquire'
+	
+	def test_class_method(self):
+		from marrow.task.model.task import Task
+		assert resolve(Task.submit) == 'marrow.task.model.task:Task.submit'
+		assert resolve(Task().submit) == 'marrow.task.model.task:Task.submit'
 
 
 class TestLookup(object):
